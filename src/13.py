394f9b8db6005e4591703405da8bd40411c538f4
Hans Krumsvik <hans.krumsvik@gmail.com>
2024-01-03 13:13:29 +0100

    Day 13 completed

--- src/13.py
+++ src/13.py
@@ -11,49 +11,66 @@ lines = open(inFile, "r").read().splitlines()
 R=len(lines)
 C=len(lines[0])
 
-def progresscanners():
-    for l in layers:
+def progresslayer(l):
         d,s,dir = layers[l]
         if s+dir <0 or s+dir >=d:
             layers[l][2] = -layers[l][2]
         layers[l][1] = layers[l][1] + layers[l][2]
-        
+        if layers[l][1] == 0:
+            return False
+        else:
+            return True
+
+def progresscanners():
+    allnonzero = True
+    for l in layers:
+        if not(progresslayer(l)):
+            allnonzero = False
+    return allnonzero
 
 
 # Part 1
-delay = 0
-while True:
-    layers = dict()
-    numoflayers = 0
-    for line in lines:
-        l, d = [int(x) for x in line.split(": ")]
-        layers[l]=[d, 0, 1]
-        numoflayers = l+1
 
-    cf = 0
-    cc = 0
-    ps = 0
-    while ps-delay<numoflayers:
-        if ps>=delay:
-            pd = ps-delay
-            if pd in layers and layers[pd][1]==0:
-                cf += (layers[pd][0] * pd)
-                cc += 1
-        progresscanners()
-        ps += 1
+layers = dict()
+numoflayers = 0
+for line in lines:
+    l, d = [int(x) for x in line.split(": ")]
+    layers[l]=[d, 0, 1]
+    numoflayers = l+1
 
-    if delay == 0:  
-        p1 = cf
-        print (f"Part 1: {p1}, {str(timer)}") 
-    if cc == 0:
-        break
-    delay += 1
-    if delay%1000 == 0:
-        print(delay, cc, str(timer))
+cf = 0
+ps = 0
+while ps<numoflayers:
+    if ps in layers and layers[ps][1]==0:
+        cf += (layers[ps][0] * ps)
+    progresscanners()
+    ps += 1
+
+p1 = cf
+print (f"Part 1: {p1}, {str(timer)}") 
+
 
 
 # Part 2
-p2 = delay
+
+#prepare layers
+layers = dict()
+numoflayers = 0
+for line in lines:
+    l, d = [int(x) for x in line.split(": ")]
+    layers[l]=[d, 0, 1]
+    numoflayers = l+1
+for x in range(1,numoflayers):
+    if x in layers:
+        for y in range(1,x+1):
+            progresslayer(x)
+
+delay = 0
+while True:
+    if progresscanners():
+        break
+    delay += 1
+p2 = delay +1
 print (f"Part 2: {p2}, {str(timer)}") 
 
 
