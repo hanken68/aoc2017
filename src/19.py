51393458fa5753c1a6792085db6297553ef63594
Hans Krumsvik <hans.krumsvik@gmail.com>
2024-01-04 20:41:54 +0000

    Day19

--- src/19.py
+++ src/19.py
@@ -1,5 +1,5 @@
 import aoc
-test = False
+test = True
 timer = aoc.executionTime()
 #directions = ((-1,0),(1,0),(0,-1),(0,1),(-1,-1),(1,-1),(-1,1),(1,1))  # with diagonals
 directions = ((-1,0),(1,0),(0,-1),(0,1))
@@ -7,16 +7,35 @@ directions = ((-1,0),(1,0),(0,-1),(0,1))
 inFile = "src/inputs/19s.txt" if test else "src/inputs/19.txt"
 p1 = p2 = 0
 
-lines = open(inFile, "r").read().splitlines()
-R=len(lines)
-C=len(lines[0])
+grid = open(inFile, "r").read().splitlines()
+R=len(grid)
+C=len(grid[0])
 
+c = grid[0].index("|")
+r = 0
+d = (1,0)
 # Part 1
-for line in lines:
-    for c in line:
-        print(c)
+text = ""
+while True:
+    l = grid[r][c]
+    print(r,c,l,d, text)
+    if l == "+":
+        for dr, dc in directions:
+            rr = r + dr
+            cc = c + dc
+            if (dr,dc) == d or (dr == -d[0] and dc == -d[1]) or (rr < 0 or rr>=R or cc<0 or cc>=C): # only 90 degree allowed and inbounds
+                continue
+            if grid[rr][cc] != " ":
+                d=(dr, dc)
+                break
+    elif not(l in "|-"):
+        text += l
+    elif l== " ":
+        break
+    r += d[0]
+    c += d[1]
 
-print (f"Part 1: {p1}, {str(timer)}") 
+print (f"Part 1: {text}, {str(timer)}") 
 
 # Part 2
 
